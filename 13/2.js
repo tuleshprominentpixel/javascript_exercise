a23e12c0e85882f6866e2563dec9ffa008294743
tuleshprominentpixel <tulesh.g@prominentpixel.com>
2021-10-22 11:58:23 +0530

    13

---
+++ 13/2.js
@@ -0,0 +1,8 @@
+var p1=document.getElementById("gt");
+var a1=p1.offsetTop;
+console.log(a1);
+p1.scrollTo(0,50);
+function scrollByBtn()
+{
+    p1.scrollBy(0,50);
+}
---
+++ 13/3.js
@@ -0,0 +1,5 @@
+var p1=document.createElement("p");
+var d1=document.getElementById("gt");
+p1.className="para";
+p1.textContent="random class";
+d1.append(p1);
